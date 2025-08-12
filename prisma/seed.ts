d833c87ef64b2b1b806a80bc4ad820e12f4bb124
thewongdev <louis.sc@gmail.com>
2025-08-12 13:06:00 -0400

    fix: update ticket content and statuses in seed data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,18 +5,24 @@ const prisma = new PrismaClient();
 const tickets = [
   {
     title: "Ticket 1",
-    content: "This is the first ticket from the database",
+    content: "First ticket from DB.",
     status: "DONE" as const,
+    deadline: new Date().toISOString().split("T")[0],
+    bounty: 499,
   },
   {
     title: "Ticket 2",
-    content: "This is the second ticket from the database",
-    status: "IN_PROGRESS" as const,
+    content: "Second ticket from DB.",
+    status: "OPEN" as const,
+    deadline: new Date().toISOString().split("T")[0],
+    bounty: 399,
   },
   {
     title: "Ticket 3",
-    content: "This is the third ticket from the database",
-    status: "OPEN" as const,
+    content: "Third ticket from DB.",
+    status: "IN_PROGRESS" as const,
+    deadline: new Date().toISOString().split("T")[0],
+    bounty: 599,
   },
 ];
 
